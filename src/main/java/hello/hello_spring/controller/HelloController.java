9096f2ca08862092c18828845f2086c93ed341f1
GwanWoo Jeong <rhksdn1654@gmail.com>
2024-07-19 15:38:34 +0900

    course03 view

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -9,7 +9,9 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
-        model.addAttribute("data", "hello!!");
-        return "hello";
+        model.addAttribute("data", "spring!!");
+        return "hello"; // 컨트롤러에서 리턴 값으로 문자를 반환하면 뷰 리졸버 (viewResolver) 가 화면을 찾아서 처리한다.
+        //스프링 부트 템플릿엔진 기본 viewName 매핑
+        //resources/templates/hello.html
     }
 }
